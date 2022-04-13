ad053f30dac3cd26017ac8a60985e2e259652a2c
svv <vvsh1978@mail.ru>
2022-04-13 15:58:54 +0500

    feat: make lesson15 hard

--- script.js
+++ script.js
@@ -5,28 +5,76 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
     this.bg = bg;
     this.fontSize = fontSize;
     this.text = text;
-
-    this.createEl = function () {
-        let element = '';
-        if (this.selector[0] === '.') {
-            element = document.createElement('div');
-            element.classList.add = this.selector
-        }
-        if (this.selector[0] === '#') {
-            element = document.createElement('p');
-            element.id = this.selector
-        }
-        element.textContent = this.text;
-        element.style.height = this.height;
-        element.style.width = this.width;
-        element.style.backgroundColor = this.bg;
-        element.style.fontSize = this.fontSize;
-        document.body.append(element)
-    }
 }
 
-const newElement1 = new DomElement('.class-element', '200px', '400px', 'red', '24px', 'Это блочный элемент DIV');
-const newElement2 = new DomElement('#id-element', '200px', '600px', 'blue', '40px', 'Это параграф');
-newElement1.text = 'Это новый текст для блочного элемента DIV'
-newElement1.createEl();
-newElement2.createEl()
+DomElement.prototype.createEl = function () {
+    let element = '';
+    if (this.selector[0] === '.') {
+        element = document.createElement('div');
+        element.classList.add = this.selector
+    }
+    if (this.selector[0] === '#') {
+        element = document.createElement('p');
+        element.id = this.selector
+    }
+    element.textContent = this.text;
+    element.style.height = this.height;
+    element.style.width = this.width;
+    element.style.backgroundColor = this.bg;
+    element.style.fontSize = this.fontSize;
+    document.body.append(element);
+    return element
+}
+
+const squareElement = new DomElement('.square', '100px', '100px', 'magenta')
+squareElement.position = 'absolute'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const element = squareElement.createEl();
+    element.style.position = squareElement.position;
+
+    console.dir(element);
+
+    document.addEventListener('keydown', (event) => {
+        let left = parseInt(getComputedStyle(element).left);
+        let top = parseInt(getComputedStyle(element).top);
+        switch (event.key) {
+            case 'ArrowUp': {
+                top -= 10
+                element.style.top = top + 'px'
+                break;
+            }
+            case 'ArrowDown': {
+                top += 10
+                element.style.top = top + 'px'
+                break;
+            }
+            case 'ArrowLeft': {
+                left -= 10
+                element.style.left = left + 'px'
+                break;
+            }
+            case 'ArrowRight': {
+                left += 10
+                element.style.left = left + 'px'
+                break;
+            }
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+// const newElement1 = new DomElement('.class-element', '200px', '400px', 'red', '24px', 'Это блочный элемент DIV');
+// const newElement2 = new DomElement('#id-element', '200px', '600px', 'blue', '40px', 'Это параграф');
+// newElement1.text = 'Это новый текст для блочного элемента DIV'
+// newElement1.createEl();
+// newElement2.createEl()
+
